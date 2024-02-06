55ce2ebfc83ec92794993307a3d3b79cc9952487
dantheman8300 <daniel.7039@hotmail.com>
2024-02-05 17:38:19 -0800

    window

--- move-mania-app/src/app/crash/chatWindow.tsx
+++ move-mania-app/src/app/crash/chatWindow.tsx
@@ -46,22 +46,30 @@ export default function ChatWindow() {
   }
 
   return (
-    <div>
-      <h1>Chat</h1>
-      <div>
+    <div className="bg-neutral-950 border border-neutral-700 h-[300px] w-[700px] flex flex-col items-center py-1 px-2 gap-2" >
+      <div className="grow flex flex-col items-start justify-end w-full gap-1 overflow-hidden">
         {chatMessages.map((message, index) => (
-          <div key={index}>
-            <strong>{message.author}</strong>: {message.message}
-          </div>
+          <ChatBubble key={index} message={message} />
         ))}
       </div>
-      <input type="text" placeholder="Type a message..." onKeyPress={(e) => {
-        if (e.key === 'Enter') {
-          const input = e.target as HTMLInputElement;
-          onSendMessage(input.value);
-          input.value = '';
-        }
-      }} />
+      <div className="w-full min-h-[30px] flex flex-row gap-1">
+        <input className="bg-neutral-800 grow text-white outline-none ps-2" type="text" placeholder="Type a message..." onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            const input = e.target as HTMLInputElement;
+            onSendMessage(input.value);
+            input.value = '';
+          }
+        }} />
+        <button className="border border-green-400 text-white w-[50px]">Send</button>
+      </div>
+    </div>
+  )
+}
+
+function ChatBubble({ message }: { message: ChatMessage }) {
+  return (
+    <div className="bg-neutral-800 w-full px-2 py-1">
+      <strong className="text-green-400">{message.author}:</strong> <span className="text-white">{message.message}</span>
     </div>
   )
 }
--- move-mania-app/tailwind.config.js
+++ move-mania-app/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
