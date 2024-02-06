80c37f16de635bc9497cfd62f92fdc84c327635b
dantheman8300 <daniel.7039@hotmail.com>
2024-02-06 14:37:48 -0800

    layout

--- move-mania-app/src/app/crash/page.tsx
+++ move-mania-app/src/app/crash/page.tsx
@@ -1,3 +1,4 @@
+import CrashChart from "@/components/CrashChart.client";
 import ChatWindow from "./chatWindow";
 import ControlCenter from "./controlCenter";
 import GameScreen from "./gameScreen";
@@ -6,14 +7,19 @@ import PlayerList from "./playerList";
 
 export default function CrashPage() {
   return (
-    <div className="h-full min-h-screen w-full bg-neutral-950 text-white">
-      <span>
-        Crash page
-      </span>
-      <GameScreen />
-      <ControlCenter />
-      <PlayerList />
-      <ChatWindow />
+    <div className="h-full w-full bg-neutral-950 text-white flex flex-col items-center py-4">
+      <div className="flex flex-row items-start w-full h-[700px] gap-2 px-2">
+        <div className=" w-[75%] flex flex-col items-center gap-2 h-full border border-neutral-700">
+          <div className="h-[90%]">
+            <GameScreen />
+            {/* <CrashChart /> */}
+          </div>
+          <ControlCenter />
+        </div>
+        <div className="h-full ">
+          <PlayerList />
+        </div>
+      </div>
     </div>
   )
 }
