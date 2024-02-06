ee57cb12fbd3526e2131864fd474a0c28f3a1f8f
dantheman8300 <daniel.7039@hotmail.com>
2024-02-06 15:52:57 -0800

    most of manual control center

--- move-mania-app/src/app/crash/page.tsx
+++ move-mania-app/src/app/crash/page.tsx
@@ -9,12 +9,14 @@ export default function CrashPage() {
   return (
     <div className="h-full w-full bg-neutral-950 text-white flex flex-col items-center py-4">
       <div className="flex flex-row items-start w-full h-[700px] gap-2 px-2">
-        <div className=" w-[75%] flex flex-col items-center gap-2 h-full border border-neutral-700">
-          <div className="h-[90%]">
+        <div className=" w-[75%] flex flex-col items-center gap-2 h-full border border-neutral-700 p-2">
+          <div className="h-[90%] w-full p-4">
             <GameScreen />
             {/* <CrashChart /> */}
           </div>
-          <ControlCenter />
+          <div className="max-h-[100px] h-full w-full">
+            <ControlCenter />
+          </div>
         </div>
         <div className="h-full max-w-[300px]">
           <PlayerList />
