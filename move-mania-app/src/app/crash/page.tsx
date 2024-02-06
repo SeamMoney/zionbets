8e4f68ba39a17518a4047cf5393acf1c1027e9b5
dantheman8300 <daniel.7039@hotmail.com>
2024-02-06 15:55:45 -0800

    added leaderboard layout

--- move-mania-app/src/app/crash/page.tsx
+++ move-mania-app/src/app/crash/page.tsx
@@ -7,8 +7,8 @@ import PlayerList from "./playerList";
 
 export default function CrashPage() {
   return (
-    <div className="h-full w-full bg-neutral-950 text-white flex flex-col items-center py-4">
-      <div className="flex flex-row items-start w-full h-[700px] gap-2 px-2">
+    <div className="bg-neutral-950 text-white flex flex-col items-center px-2 gap-2 py-4">
+      <div className="flex flex-row items-start w-full h-[700px] gap-2 ">
         <div className=" w-[75%] flex flex-col items-center gap-2 h-full border border-neutral-700 p-2">
           <div className="h-[90%] w-full p-4">
             <GameScreen />
@@ -21,6 +21,9 @@ export default function CrashPage() {
         <div className="h-full max-w-[300px]">
           <PlayerList />
         </div>
+      </div>
+      <div className="border border-neutral-700 p-2 w-full h-[800px]">
+
       </div>
     </div>
   )
