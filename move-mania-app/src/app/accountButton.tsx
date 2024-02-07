28c802638173f448a66a3288ce16ed33bd7235bb
dantheman8300 <daniel.7039@hotmail.com>
2024-02-07 08:11:45 -0800

    edit profile - no functionality yet

--- move-mania-app/src/app/accountButton.tsx
+++ move-mania-app/src/app/accountButton.tsx
@@ -12,12 +12,15 @@ import {
 } from "@/components/ui/sheet"
 import { doesUserExist, getUser, setUpAndGetUser, setUpUser } from "@/lib/api";
 import { User } from "@/lib/schema";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { getSession, signIn, signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 export default function AccountButton() {
 
   const [account, setAccount] = useState<User | null>(null)
+
+  const [privateKeyVisible, setPrivateKeyVisible] = useState(false)
 
   useEffect(() => {
     getSession().then(session => {
@@ -50,7 +53,7 @@ export default function AccountButton() {
             {account ? account.username : 'Sign in'}
           </button>
         </SheetTrigger>
-        <SheetContent>
+        <SheetContent className="w-96">
           {
             !account ? (
               <div className="flex items-center justify-center h-32">
@@ -64,26 +67,58 @@ export default function AccountButton() {
                     Make changes to your profile here. Click save when you're done.
                   </SheetDescription>
                 </SheetHeader>
-                  <button onClick={onSignIn}>sign in</button>
-                  <br />
-                  <button onClick={() => signOut()}>sign out</button>
                 <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <label htmlFor="name" className="text-right">
-                      Name
+                  <div className="border border-neutral-700 flex flex-row justify-between px-4 py-2">
+                    <label htmlFor="email" className="text-left font-mono">
+                      Email
                     </label>
-                    <input id="name" value="Pedro Duarte" className="col-span-3" />
+                    <span className="font-mono opacity-50 flex flex-row justify-end items-center gap-1">
+                      <input id="email" placeholder={account.email} className="bg-transparent border-none outline-none text-right text-ellipsis" />
+                    </span>
                   </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <label htmlFor="username" className="text-right">
+                  <div className="border border-neutral-700 flex flex-row justify-between px-4 py-2">
+                    <label htmlFor="username" className="text-left font-mono">
                       Username
                     </label>
-                    <input id="username" value="@peduarte" className="col-span-3" />
+                    <span className="font-mono opacity-50 flex flex-row justify-center items-center gap-1">
+                      <input id="username" placeholder={account.username} className="bg-transparent border-none outline-none text-right text-ellipsis" />
+                    </span>
+                  </div>
+                  <div className="border border-neutral-700 flex flex-row justify-between px-4 py-2">
+                    <label htmlFor="profile_pic" className="text-left font-mono">
+                      Profile picture
+                    </label>
+                    <span className="font-mono opacity-50 flex flex-row justify-center items-center gap-1">
+                      <input id="profile_pic" placeholder={account.image} className="bg-transparent border-none outline-none text-right text-ellipsis" />
+                    </span>
+                  </div>
+                  <div className="border border-neutral-700 flex flex-row justify-between px-4 py-2">
+                    <label htmlFor="public_address" className="text-left font-mono">
+                      Public address
+                    </label>
+                    <span className="font-mono opacity-50 flex flex-row justify-center items-center gap-1">
+                      <input id="public_address" disabled value={account.public_address} className="bg-transparent border-none outline-none text-right text-ellipsis" />
+                    </span>
+                  </div>
+                  <div className="border border-neutral-700 flex flex-row justify-between px-4 py-2">
+                    <label htmlFor="private_key" className="font-mono font-light w-[100px]">
+                      Private key
+                    </label>
+                    <span className="font-mono opacity-50 flex flex-row justify-center items-center gap-1">
+                      <input id="private_key" hidden={!privateKeyVisible} disabled value={account.private_key} className="bg-transparent border-none outline-none text-right text-ellipsis w-[170px]" />
+                      {
+                        !privateKeyVisible ? (
+                          <EyeIcon className="cursor-pointer" onClick={() => setPrivateKeyVisible(true)} />
+                        ) : (
+                          <EyeOffIcon className="cursor-pointer" onClick={() => setPrivateKeyVisible(false)} />
+                        )
+                      }
+                    </span>
                   </div>
                 </div>
                 <SheetFooter>
                   <SheetClose asChild>
-                    <button type="submit">Save changes</button>
+                    <button type="submit" className="border border-green-500 px-6 py-1 text-green-500">Save changes</button>
                   </SheetClose>
                 </SheetFooter>
               </>
