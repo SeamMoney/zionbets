498f6231ac5a7d2eb04b663a3cc2e3dcebdaf8c7
Max Mohammadi <maxmohammadi@gmail.com>
2024-02-23 21:02:41 -0800

    add rocket to graph

--- move-mania-app/src/components/CrashChart.client.tsx
+++ move-mania-app/src/components/CrashChart.client.tsx
@@ -2,17 +2,17 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import "../app/globals.css";
-import { channel } from 'diagnostics_channel';
 
 interface CrashChartProps {
   startAnimation: boolean;
   crashPoint?: number;
 }
 
-function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
+function CrashChart({ startAnimation, crashPoint }: CrashChartProps) {
   const margin = { top: 20, right: 20, bottom: 30, left: 40 };
   const d3Container = useRef<SVGSVGElement | null>(null);
   const animationFrameId = useRef<number | null>(null);
+  const rocketRef = useRef<SVGImageElement | null>(null);
 
   useEffect(() => {
     if (!startAnimation || !d3Container.current) return;
@@ -20,6 +20,8 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
     const containerRect = d3Container.current.getBoundingClientRect();
     const width = containerRect.width - margin.left - margin.right;
     const height = containerRect.height - margin.top - margin.bottom;
+    const rocketWidth = 100;
+    const rocketHeight = 100;
 
     const svg = d3.select(d3Container.current)
       .attr('viewBox', `0 0 ${containerRect.width} ${containerRect.height}`)
@@ -35,7 +37,6 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
     const xAxisGroup = svg.append('g')
       .attr('class', 'x axis')
       .attr('transform', `translate(0, ${height})`);
-
     const yAxisGroup = svg.append('g')
       .attr('class', 'y axis');
 
@@ -43,7 +44,6 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
     yAxisGroup.call(yAxis);
 
     let data: [number, number][] = [];
-
     const line = d3.line<[number, number]>()
       .x(d => xScale(d[0]))
       .y(d => yScale(d[1]));
@@ -51,11 +51,9 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
     let time = 0;
 
     const updateChart = () => {
-
       if (crashPoint == null) return;
 
       const base = 1.5;
-
       time += 0.001;
       const newYValue = (Math.pow(base, time) - 1) * (crashPoint / (Math.pow(base, 10) - 1));
       if (newYValue >= crashPoint) {
@@ -65,8 +63,6 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
 
       const newDataPoint: [number, number] = [time, newYValue];
       data.push(newDataPoint);
-
-      // if (data.length > num) data.shift();
 
       xScale.domain([Math.max(time - 10, 0), time]);
       yScale.domain([0, d3.max(data, d => d[1]) || crashPoint]);
@@ -79,6 +75,44 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
         .join('path')
         .attr('class', 'line')
         .attr('d', line);
+
+      if (data.length === 1) {
+        // Append the rocket image when the first data point is created
+        const rocketUrl = '/rocket.png';
+        const rocketWidth = 100;
+        const rocketHeight = 100;
+        rocketRef.current = svg.append('image')
+          .attr('xlink:href', rocketUrl)
+          .attr('width', rocketWidth)
+          .attr('height', rocketHeight)
+          .attr('x', xScale(newDataPoint[0]) - rocketWidth / 2)
+          .attr('y', yScale(newDataPoint[1]) - rocketHeight / 2)
+          .node();
+      } else if (data.length > 1) {
+        const lastPoint = data[data.length - 1];
+        const secondLastPoint = data[data.length - 2];
+
+        // Calculate the angle
+        const deltaY = yScale(secondLastPoint[1]) - yScale(lastPoint[1]);
+        const deltaX = xScale(secondLastPoint[0]) - xScale(lastPoint[0]);
+        let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+        angle += 245;
+
+        // Update the rocket's position and rotation
+        if (rocketRef.current) {
+          const rocketX = xScale(lastPoint[0]) - rocketWidth / 2;
+          const rocketY = yScale(lastPoint[1]) - rocketHeight / 2;
+
+          const rotateX = rocketX + rocketWidth / 2;
+          const rotateY = rocketY + rocketHeight / 2;
+
+          d3.select(rocketRef.current)
+            .attr('x', rocketX)
+            .attr('y', rocketY)
+            .attr('transform', `rotate(${angle},${rotateX},${rotateY})`);
+        }
+      }
 
       time += 0.1;
       animationFrameId.current = requestAnimationFrame(updateChart);
@@ -98,4 +132,4 @@ function EmptyCrashChart ({ startAnimation, crashPoint }: CrashChartProps) {
   );
 };
 
-export default EmptyCrashChart;
+export default CrashChart;
