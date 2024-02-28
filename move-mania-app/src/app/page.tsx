a61bf670c9d2d46e3c74433c79bf13ec0997e132
dantheman8300 <daniel.7039@hotmail.com>
2024-02-28 10:16:58 -0800

    progress with data generation

--- move-mania-app/src/app/page.tsx
+++ move-mania-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from "next/image";
 import CrashChart from "../components/CrashChart.client";
+import CandlestickChart from '@/components/CandlestickChart.client';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <CrashChart startAnimation={true} crashPoint={50} />
+      {/* <CrashChart startAnimation={true} crashPoint={50} /> */}
+      {/* <CandlestickChart /> */}
     </main>
   );
 }
