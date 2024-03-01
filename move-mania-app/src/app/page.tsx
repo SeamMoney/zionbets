82aa275410ec7ee6905165e11a930f7d20df6232
dantheman8300 <daniel.7039@hotmail.com>
2024-03-01 12:47:27 -0800

    removed use of old code

--- move-mania-app/src/app/page.tsx
+++ move-mania-app/src/app/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Image from "next/image";
-import CrashChart from "../components/CrashChart.client";
-import CandlestickChart from '@/components/CandlestickChart.client';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {/* <CrashChart startAnimation={true} crashPoint={50} /> */}
-      {/* <CandlestickChart /> */}
     </main>
   );
 }
