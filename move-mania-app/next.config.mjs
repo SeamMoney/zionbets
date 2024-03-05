e4c81a31e92b3e727ca548ef3ce242c3311c0876
dantheman8300 <daniel.7039@hotmail.com>
2024-03-04 19:15:32 -0800

    pwa!

--- move-mania-app/next.config.mjs
+++ move-mania-app/next.config.mjs
@@ -14,4 +14,16 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+import withPWA from "next-pwa";
+
+// Configuration object tells the next-pwa plugin 
+const nextPWAConfig = {
+  dest: "public", // Destination directory for the PWA files
+  // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+};
+
+const withPWAConfig = withPWA(nextPWAConfig);
+
+export default withPWAConfig;
