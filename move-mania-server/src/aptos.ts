a2badd8619baaf91f8f920742165e64b1a96939c
dantheman8300 <daniel.7039@hotmail.com>
2024-03-13 12:52:21 -0700

    Fix delay calculation in endGame function

--- move-mania-server/src/aptos.ts
+++ move-mania-server/src/aptos.ts
@@ -90,8 +90,8 @@ export async function endGame(house_secret: string, salt: string, crashTime: num
   const adminAccount = getAdminAccount();
 
   // If the crash time is in the future, then wait until the crash time to end the game
-  if (crashTime >= Date.now()) {
-    delay(crashTime + 1000 - Date.now());
+  if (crashTime + 500 >= Date.now()) {
+    delay(crashTime + 500 - Date.now());
   }
     
 
