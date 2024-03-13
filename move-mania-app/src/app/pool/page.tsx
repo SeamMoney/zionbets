96b2a95152077ac3664f3d6b9bd5ee0d41d1fc3a
dantheman8300 <daniel.7039@hotmail.com>
2024-03-12 19:07:21 -0700

    page init

---
+++ move-mania-app/src/app/pool/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function PoolPage() {
+
+  return (
+    <div className="bg-[#020202] text-white flex flex-col items-center px-2 gap-2 py-4 bg-noise w-full">
+      <div className="flex flex-col items-center w-full gap-2">
+        <div className="w-full flex flex-col items-center justify-between border border-neutral-700 overflow-hidden">
+          pool page
+        </div>
+      </div>
+    </div>
+  );
+}
