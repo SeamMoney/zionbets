24c67edf9737e67b3e3db8d5082b62e3b8a02a3e
dantheman8300 <daniel.7039@hotmail.com>
2024-03-13 12:57:44 -0700

    Add console log to check crash time and current time

--- move-mania-server/src/aptos.ts
+++ move-mania-server/src/aptos.ts
@@ -90,6 +90,7 @@ export async function endGame(house_secret: string, salt: string, crashTime: num
   const adminAccount = getAdminAccount();
 
   // If the crash time is in the future, then wait until the crash time to end the game
+  console.log(crashTime, Date.now());
   if (crashTime + 500 >= Date.now()) {
     delay(crashTime + 500 - Date.now());
   }
