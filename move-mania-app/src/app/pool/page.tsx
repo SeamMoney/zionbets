6b56477764b1b2e6cf74bc9633543f0dca266716
dantheman8300 <daniel.7039@hotmail.com>
2024-03-15 15:34:18 -0700

    pool navigation

--- move-mania-app/src/app/pool/page.tsx
+++ move-mania-app/src/app/pool/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PoolGraph from "./poolGraph";
 import PoolInfo from "./poolInfo";
 import PoolModal from "./poolModal";
@@ -6,7 +7,10 @@ import PoolModal from "./poolModal";
 export default function CrashPage() {
 
   return (
-    <div className="bg-[#020202] text-white flex flex-col items-center px-2 gap-2 py-4 bg-noise w-full">
+    <div className="bg-[#020202] text-white flex flex-col items-center px-2 pt-2 gap-2 bg-noise w-full">
+      <Link href='/' className="w-full underline opacity-70">
+        Back to Game
+      </Link>
       <div className="flex flex-col items-center w-full gap-2">
         <div className="w-full">
           <PoolGraph />
