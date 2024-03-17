a7ae739ee68e22ab4a0aa50b7ad6d26cb23e13d6
dantheman8300 <daniel.7039@hotmail.com>
2024-03-17 09:12:08 -0700

    build ready

--- move-mania-app/src/app/pool/poolInfo.tsx
+++ move-mania-app/src/app/pool/poolInfo.tsx
@@ -6,7 +6,7 @@ export default function PoolInfo() {
         Crash Liquidity Pool
       </h1>
       <p className="w-full max-w-lg text-sm opacity-50">
-      As a Liquidity Provider (LP), you mint exclusive shares that symbolize your stake in ZionBets. These LP shares are your pass to a continuous stream of revenue generated from our suite of games. By providing liquidity, you are directly contributing to the decentralization and robustness of the ZionBets protocol. First, you supply funds to the pool and, in return, receive LP shares, which grow in value as the house's profits climb. Then, when you're ready to cash out, you burn these shares and withdraw your stake + profit.
+      As a Liquidity Provider (LP), you mint exclusive shares that symbolize your stake in ZionBets. These LP shares are your pass to a continuous stream of revenue generated from our suite of games. By providing liquidity, you are directly contributing to the decentralization and robustness of the ZionBets protocol. First, you supply funds to the pool and, in return, receive LP shares, which grow in value as the house&apos;s profits climb. Then, when you&apos;re ready to cash out, you burn these shares and withdraw your stake + profit.
       </p>
     </div>
   )
