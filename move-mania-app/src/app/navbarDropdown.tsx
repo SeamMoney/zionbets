073ea1889f53cc1f7d0339459587310ddd3090fc
dantheman8300 <daniel.7039@hotmail.com>
2024-03-26 08:51:17 -0700

    more interactivity and ux improvements

--- move-mania-app/src/app/navbarDropdown.tsx
+++ move-mania-app/src/app/navbarDropdown.tsx
@@ -43,7 +43,7 @@ export default function NavbarDropdown() {
       {
         !account && 
         <button
-          className="bg-white px-6 py-1 text-neutral-950"
+          className="bg-white px-6 py-1 text-neutral-950 active:scale-95 active:opacity-50 transition-transform"
           onClick={() => {
             signIn("google");
           }}
