1de752bf57bbb849f782c07c72b6ab552acb8dec
dantheman8300 <daniel.7039@hotmail.com>
2024-03-28 18:15:03 -0700

    suspense

--- move-mania-app/src/app/navbarDropdown.tsx
+++ move-mania-app/src/app/navbarDropdown.tsx
@@ -14,7 +14,7 @@ import { Ellipsis } from "lucide-react";
 import { getSession, signIn } from "next-auth/react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 
 export default function NavbarDropdown() {
@@ -43,6 +43,7 @@ export default function NavbarDropdown() {
   }, []);
 
   return (
+    <Suspense>
     <div className="flex flex-row items-center gap-2">
       {
         !account && 
@@ -70,5 +71,6 @@ export default function NavbarDropdown() {
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
+    </Suspense>
   )
 }
