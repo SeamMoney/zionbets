c876893dfce2ebdc81d2c0867c2fdfb40660eeae
dantheman8300 <daniel.7039@hotmail.com>
2024-04-16 22:31:13 -0700

    stable, non-spinning magic auth without blockchain stuff

--- move-mania-app/src/app/MagicProvider.tsx
+++ move-mania-app/src/app/MagicProvider.tsx
@@ -16,19 +16,22 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { cn } from "@/lib/utils";
+import { MagicAptosWallet } from "@magic-ext/aptos";
 
 interface MagicProviderProps {
   isLoggedIn: boolean | null;
   userInfo: UserV2 | null;
   setIsLoggedIn: (isLoggedIn: boolean) => void;
-  publicAddress: string | null
+  publicAddress: string | null,
+  aptosWallet: MagicAptosWallet | null
 }
 
 export const magicContext = createContext<MagicProviderProps>({
   isLoggedIn: null,
   userInfo: null,
   setIsLoggedIn: () => {},
-  publicAddress: null
+  publicAddress: null,
+  aptosWallet: null
 }); 
 
 
@@ -37,43 +40,73 @@ export default function MagicProvider({ children }: { children: React.ReactNode 
   const [ isLoggedIn, setIsLoggedIn ] = useState<boolean | null>(null);
   const [ userInfo, setUserInfo ] = useState<UserV2 | null>(null);
   const [ publicAddress, setPublicAddress ] = useState<string | null>(null);
+  const [ aptosWallet, setAptosWallet ] = useState<MagicAptosWallet | null>(null);
+
+  // useEffect(() => {
+  //   if (!magic) {
+  //     console.error('Magic not yet initialized');
+  //     return;
+  //   }
+  //   console.log('magic', magic)
+  //   magic.user.isLoggedIn().then((isLoggedIn) => {
+  //     console.log('isLoggedIn', isLoggedIn)
+  //     setIsLoggedIn(isLoggedIn);
+  //   //   if (isLoggedIn) {
+  //   //     if (!magic) {
+  //   //       console.error('Magic not yet initialized');
+  //   //       return;
+  //   //     }
+  //   //     magic.aptos.getAccountInfo().then((userInfo) => {
+  //   //       console.log('userInfo', userInfo)
+  //   //       setPublicAddress(userInfo.address);
+          
+  //   //       setUpAndGetUser({
+  //   //         address: userInfo.address,
+  //   //       }).then((user) => {
+  //   //         if (user) {
+  //   //           setUserInfo(user);
+  //   //         }
+  //   //       });
+  //   //     });
+  //   //   }
+  //   });
+  // }, [])
 
   useEffect(() => {
-    if (!magic) {
-      console.error('Magic not yet initialized');
-      return;
-    }
-    console.log('magic', magic)
-    magic.user.isLoggedIn().then((isLoggedIn) => {
-      console.log('isLoggedIn', isLoggedIn)
-      setIsLoggedIn(isLoggedIn);
-      if (isLoggedIn) {
-        if (!magic) {
-          console.error('Magic not yet initialized');
-          return;
-        }
-        magic.aptos.getAccountInfo().then((userInfo) => {
-          console.log('userInfo', userInfo)
-          setPublicAddress(userInfo.address);
-          
-          setUpAndGetUser({
-            address: userInfo.address,
-          }).then((user) => {
-            if (user) {
-              setUserInfo(user);
-            }
-          });
+    magic.user.isLoggedIn().then(async (magicIsLoggedIn: boolean) => {
+      console.log('magicIsLoggedIn', magicIsLoggedIn)
+      setIsLoggedIn(magicIsLoggedIn);
+
+      if (magicIsLoggedIn) {
+        const magicAptosWallet = new MagicAptosWallet(magic, {
+          connect: async () => {
+            return await magic.aptos.getAccountInfo();
+          },
+        });
+        setAptosWallet(magicAptosWallet);
+        console.log('magicAptosWallet', magicAptosWallet)
+
+        const accountInfo = await magicAptosWallet.account();
+        setPublicAddress(accountInfo.address);
+        console.log('accountInfo', accountInfo)
+        setUpAndGetUser({
+          address: accountInfo.address,
+        }).then((user) => {
+          if (user) {
+            setUserInfo(user);
+          }
         });
       }
     });
-  })
+  }, []);
 
   return (
     <magicContext.Provider value={{
       isLoggedIn,
       userInfo,
       setIsLoggedIn,
-      publicAddress
+      publicAddress,
+      aptosWallet
     }}>
       {children}
     </magicContext.Provider>
