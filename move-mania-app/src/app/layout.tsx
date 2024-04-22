be1fd4f9dd47dcd0b54b27c0f80ecc0fd82a15bc
dantheman8300 <daniel.7039@hotmail.com>
2024-04-21 17:09:58 -0700

    loading spinner

--- move-mania-app/src/app/layout.tsx
+++ move-mania-app/src/app/layout.tsx
@@ -13,7 +13,6 @@ import logo from "@/../public/zionbet_nobackground.png"
 import NavbarDropdown from "./navbarDropdown";
 import { Suspense } from "react";
 import CrashProvider from "./CrashProvider";
-import MagicProvider from "./MagicProvider";
 import KeylessProvider from "./KeylessProvider";
 
 const baijamjuree = Bai_Jamjuree({
@@ -52,42 +51,40 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
       <body className={baijamjuree.className + " text-white bg-[#020202] bg-noise"}>
         <KeylessProvider>
-          <MagicProvider>
-            <CrashProvider>
-                <div className="flex h-full min-h-screen overflow-hidden">
-                  <nav className="fixed w-full h-12 z-30 px-2 border-b border-neutral-700 bg-[#020202] bg-noise">
-                    <div className="flex flex-row items-center justify-between w-full h-full">
-                      <div className="flex flex-row items-center gap-4">
-                        <div className="flex flex-row items-center justify-start">
-                          <Link href='/'><Image src={logo} className="" alt="" width={200} height={200} /></Link>
-                          {/* <Link href='/' className="text-white text-2xl font-bold">ZION</Link> */}
-                        </div>
-                        {/* <Link href='/pool' className="bg-neutral-800 hover:bg-neutral-700 px-2 py-1 text-xs text-white font-semibold">
-                          Pool
-                        </Link> */}
+          <CrashProvider>
+              <div className="flex h-full min-h-screen overflow-hidden">
+                <nav className="fixed w-full h-12 z-30 px-2 border-b border-neutral-700 bg-[#020202] bg-noise">
+                  <div className="flex flex-row items-center justify-between w-full h-full">
+                    <div className="flex flex-row items-center gap-4">
+                      <div className="flex flex-row items-center justify-start">
+                        <Link href='/'><Image src={logo} className="" alt="" width={200} height={200} /></Link>
+                        {/* <Link href='/' className="text-white text-2xl font-bold">ZION</Link> */}
                       </div>
-                      <div className="flex flex-row gap-4 items-center">
-                        <BalanceButton />
-                        {/* <AccountButton /> */}
-                        <Suspense>
-                          <NavbarDropdown />
-                        </Suspense>
-                      </div>
+                      {/* <Link href='/pool' className="bg-neutral-800 hover:bg-neutral-700 px-2 py-1 text-xs text-white font-semibold">
+                        Pool
+                      </Link> */}
                     </div>
-                  </nav>
-                  <div className="flex flex-col pt-12 w-full items-center">
-                    <main className="overflow-y-auto max-w-5xl">
-                      {children}
-                      <Toaster  />
-                    </main>
+                    <div className="flex flex-row gap-4 items-center">
+                      <BalanceButton />
+                      {/* <AccountButton /> */}
+                      <Suspense>
+                        <NavbarDropdown />
+                      </Suspense>
+                    </div>
                   </div>
-
-                  <aside className="fixed bottom-4 right-4">
-                    <ChatWindow />
-                  </aside>
+                </nav>
+                <div className="flex flex-col pt-12 w-full items-center">
+                  <main className="overflow-y-auto max-w-5xl">
+                    {children}
+                    <Toaster  />
+                  </main>
                 </div>
-              </CrashProvider>
-            </MagicProvider>
+
+                <aside className="fixed bottom-4 right-4">
+                  <ChatWindow />
+                </aside>
+              </div>
+            </CrashProvider>
           </KeylessProvider>
         </body>
     </html>
