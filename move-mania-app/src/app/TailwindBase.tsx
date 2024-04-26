9a48ba5cc229074361e8883b40715d63f0a4d93e
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2024-04-26 16:26:53 -0600

    start style file

---
+++ move-mania-app/src/app/TailwindBase.tsx
@@ -0,0 +1,8 @@
+export const QuestBase = `
+    flex flex-row m-10
+    rounded-xl
+    p-6
+    justify-between
+    outline outline-green-500 outline-offset-2 outline-4
+    shadow-[10px_10px_rgba(0,_158,_90,_0.5),_10px_10px_rgba(0,_158,_90,_0.5),_15px_15px_rgba(0,_158,_90,_0.5),_20px_20px_rgba(0,_158,_90,_0.5),_25px_25px_rgba(0,_158,_90,_0.5)]
+`;
