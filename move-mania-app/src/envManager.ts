131637eea4d5431951d73943cc73fd3df3e0fb0d
jack <jacklaxjk@gmail.com>
2024-05-22 16:57:26 +0000

    move to testnet

---
+++ move-mania-app/src/envManager.ts
@@ -0,0 +1,56 @@
+// src/config/EnvManager.ts
+
+interface EnvConfig {
+    NODE_URL: string;
+    CHAIN_MODE: string;
+    ZION_API_URL: string;
+    ZION_APP_URL: string;
+    DEV_MODE: string;
+    ADMIN_ACCOUNT_PRIVATE_KEY: string;
+    MODULE_ADDRESS: string;
+    CRASH_RESOURCE_ACCOUNT_ADDRESS: string;
+    LP_RESOURCE_ACCOUNT_ADDRESS: string;
+    Z_APT_RESOURCE_ACCOUNT_ADDRESS: string;
+}
+
+function getConfig(): EnvConfig {
+    const {
+        MOVEMENT_NODE,
+        APTOS_NODE,
+        CHAIN_MODE,
+        ZION_API_URL,
+        ZION_APP_URL,
+        DEV_MODE,
+        ADMIN_ACCOUNT_PRIVATE_KEY,
+        MODULE_ADDRESS,
+        CRASH_RESOURCE_ACCOUNT_ADDRESS,
+        LP_RESOURCE_ACCOUNT_ADDRESS,
+        Z_APT_RESOURCE_ACCOUNT_ADDRESS
+    } = process.env;
+
+    if (!MOVEMENT_NODE || !APTOS_NODE || !CHAIN_MODE || !DEV_MODE ||
+        !ADMIN_ACCOUNT_PRIVATE_KEY || !MODULE_ADDRESS || !CRASH_RESOURCE_ACCOUNT_ADDRESS ||
+        !LP_RESOURCE_ACCOUNT_ADDRESS || !Z_APT_RESOURCE_ACCOUNT_ADDRESS) {
+        throw new Error('One or more environment variables are not defined');
+    }
+
+    const config: EnvConfig = {
+        NODE_URL: (CHAIN_MODE === 'Aptos' ? APTOS_NODE : MOVEMENT_NODE) as string,
+        CHAIN_MODE: CHAIN_MODE,
+        ZION_API_URL: DEV_MODE === 'local' ? 'http://localhost:3008' : 'https://api.zionapi.xyz',
+        ZION_APP_URL: DEV_MODE === 'local' ? 'http://localhost:3000' : 'https://app.zion.bet',
+        DEV_MODE: DEV_MODE,
+        ADMIN_ACCOUNT_PRIVATE_KEY: ADMIN_ACCOUNT_PRIVATE_KEY as string,
+
+        MODULE_ADDRESS: MODULE_ADDRESS as string,
+        CRASH_RESOURCE_ACCOUNT_ADDRESS: CRASH_RESOURCE_ACCOUNT_ADDRESS as string,
+        LP_RESOURCE_ACCOUNT_ADDRESS: LP_RESOURCE_ACCOUNT_ADDRESS as string,
+        Z_APT_RESOURCE_ACCOUNT_ADDRESS: Z_APT_RESOURCE_ACCOUNT_ADDRESS as string
+    };
+    console.log(config)
+
+    return config;
+}
+
+
+export default getConfig;
