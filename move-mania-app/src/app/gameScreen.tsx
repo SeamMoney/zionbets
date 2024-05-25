e24452488b14323b5887baa71a8384834d6fea93
Max Mohammadi <maxmohammadi@gmail.com>
2024-05-24 22:22:26 -0700

    new countdown styling

--- move-mania-app/src/app/gameScreen.tsx
+++ move-mania-app/src/app/gameScreen.tsx
@@ -4,15 +4,16 @@ import { useContext, useEffect, useState } from "react";
 import { GameStatus } from "./controlCenter";
 import { SOCKET_EVENTS, RoundStart } from "@/lib/types";
 import { Socket, io } from "socket.io-client";
-import CountUp from "react-countup";
 import { getCurrentGame } from "@/lib/api";
 import { startRound } from "@/lib/socket";
+import CountUp from "react-countup";
 
 import { socket } from "@/lib/socket";
 
 import { gameStatusContext } from "./CrashProvider";
 import CandlestickChart from "@/components/CandlestickChart.client";
 import { calculateCurrentCrashPoint, generateChartData, generateLineChartData } from "@/lib/utils";
+import "@/app/customCountdown.css";
 
 export default function GameScreen() {
   const {
@@ -39,6 +40,34 @@ export default function GameScreen() {
     }
   }, [gameStatus]);
 
+  useEffect(() => {
+    const template = document.querySelector("#clocknumbers");
+
+    document.querySelectorAll('ul').forEach((n, i) => {
+      const clone = template.content.cloneNode(true);
+      n.appendChild(clone);
+    });
+
+    let countdown = 15;
+
+    const update = function () {
+      const seconds = countdown % 60;
+
+      document.body.style.setProperty('--seconds-p', Math.floor(seconds / 10));
+      document.body.style.setProperty('--seconds-a', seconds % 10);
+
+      if (countdown > 0) {
+        countdown--;
+      } else {
+        countdown = 15;
+      }
+
+      setTimeout(update, 1000); // Update every second
+    }
+
+    update();
+  }, []);
+
   if (gameStatus === null) {
     return (
       <div className=" h-full w-full bg-neutral-950">
@@ -48,18 +77,22 @@ export default function GameScreen() {
   } else if (gameStatus.status === "COUNTDOWN") {
     return (
       <div className=" w-full -mt-10">
-        <CountUp
-          className="relative z-10 top-12 left-5 text-green-500 text-4xl"
-          start={(gameStatus.startTime! - Date.now()) / 1000}
-          end={0}
-          duration={(gameStatus.startTime! - Date.now()) / 1000}
-          separator=" "
-          decimals={0}
-          decimal="."
-          prefix="Next round in  "
-          suffix=" s"
-          useEasing={false}
-        />
+        <main>
+          <ul className="s-p"></ul>
+          <ul className="s-a"></ul>
+        </main>
+        <template id="clocknumbers">
+          <li>0</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+        </template>
         <CandlestickChart startTime={gameStatus.startTime!} crashPoint={gameStatus.crashPoint} data={generateChartData(gameStatus.roundId, gameStatus.crashPoint)} linedata={generateLineChartData(gameStatus.roundId, gameStatus.crashPoint)} />
       </div>
     );
@@ -68,22 +101,6 @@ export default function GameScreen() {
       <div className=" w-full -mt-10">
         <div>
           <span className="relative z-10 top-12 left-5 text-green-500 text-4xl tracking-base">{currentMultiplier.toFixed(2)}x</span>
-          {/* <CountUp
-            className="relative z-10 top-12 left-5 text-green-500 text-xl"
-            start={(Date.now() - gameStatus.startTime!) / 1000 + 1}
-            end={gameStatus.crashPoint!}
-            duration={gameStatus.crashPoint! - ((Date.now() - gameStatus.startTime!) / 1000 + 1)}
-            separator=""
-            decimals={2}
-            decimal="."
-            prefix="Your portfolio is up: "
-            suffix="x"
-            useEasing={false}
-            // easingFn={(t: number, b: number, c: number, d: number) => {
-            //   console.log(t, b, c, d)
-            //   return (t==0) ? b : c * Math.pow(1.06, 10 * (t/d - 1)) + b;
-            // }}
-          /> */}
           <CandlestickChart startTime={gameStatus.startTime!} crashPoint={gameStatus.crashPoint} data={generateChartData(gameStatus.roundId, gameStatus.crashPoint)} linedata={generateLineChartData(gameStatus.roundId, gameStatus.crashPoint)} />
         </div>
       </div>
@@ -93,7 +110,7 @@ export default function GameScreen() {
       <div className=" w-full -mt-10">
         <div>
           <CountUp
-          className="relative z-10 top-12 left-5 text-green-500 text-4xl"
+            className="relative z-10 top-12 left-5 text-green-500 text-4xl"
             start={gameStatus.crashPoint!}
             end={gameStatus.crashPoint!}
             duration={0}
