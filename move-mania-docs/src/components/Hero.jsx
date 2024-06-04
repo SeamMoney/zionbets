87acf4b132f22755ecc9995f5fa6b7f3ed543c00
Max Mohammadi <maxmohammadi@gmail.com>
2024-06-03 22:58:53 -0700

    add docs

---
+++ move-mania-docs/src/components/Hero.jsx
@@ -0,0 +1,186 @@
+import { Fragment } from 'react'
+import Image from 'next/image'
+import clsx from 'clsx'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import styled, { css } from 'styled-components'
+
+import { Button } from '@/components/Button'
+import { HeroBackground } from '@/components/HeroBackground'
+import blurCyanImage from '@/images/blur-cyan.png'
+import blurIndigoImage from '@/images/blur-indigo.png'
+
+const codeLanguage = 'javascript'
+const code = `module seam::market {
+  // create vaults
+
+  // vaults implement strategies
+
+  // strategies are implemented 
+  // ...as their own modules,
+  // ...passed in as parameters
+
+  // LPs deposit into strategy &
+  // ...receive an NFT position
+}
+`
+
+const tabs = [
+  { name: 'Seam.move', isActive: true },
+  { name: 'BaseStrategy.move', isActive: false },
+]
+
+
+    // #6dc1dc 13.32%,
+    // #a690fc 28.55%,
+    // #fc96bb 55.55%,
+    // #ffc397 84.12%
+
+
+function TrafficLightsIcon(props) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 42 10" fill="none" {...props}>
+      <circle cx="5" cy="5" r="4.5" />
+      <circle cx="21" cy="5" r="4.5" />
+      <circle cx="37" cy="5" r="4.5" />
+    </svg>
+  )
+}
+
+export function Hero() {
+  return (
+    <div className="overflow-hidden bg-black dark:-mb-32 dark:mt-[-4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
+      <div className="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+          <div className="relative z-10 md:text-center lg:text-left">
+            <Image
+              className="absolute bottom-full right-full -mr-72 -mb-96 opacity-50"
+              src={blurCyanImage}
+              alt=""
+              width={530}
+              height={530}
+              unoptimized
+              priority
+            />
+            <div className="relative">
+              <p class="text-white font-display text-5xl tracking-tight font-bold">Seaming together</p>
+              <p className="font-bold inline bg-gradient-to-r from-[#6dc1dc] via-[#fc96bb] to-[#ffc397] h-16 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                composable DeFi products
+              </p>
+              <p className="mt-3 text-2xl tracking-tight text-slate-400">
+                Seam pools are multi-protocol composite positions for staking, liquidity pools, lending, on-chain order-books, and other derivatives.
+              </p>
+              <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
+                <Button href="https://www.github.com/SeamMoney" variant="secondary">
+                  View on GitHub
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="relative lg:static xl:pl-10">
+            <div className="relative">
+              <Image
+                className="absolute -top-25 -right-64"
+                src={blurCyanImage}
+                alt=""
+                width={530}
+                height={530}
+                unoptimized
+                priority
+              />
+              <Image
+                className="absolute -bottom-20 -right-44"
+                src={blurIndigoImage}
+                alt=""
+                width={567}
+                height={567}
+                unoptimized
+                priority
+              />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10 blur-lg" />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10" />
+              <div className="relative rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur border border-white border-dashed border-[3px]">
+                <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-sky-300/0 via-sky-300/70 to-sky-300/0" />
+                <div className="absolute -bottom-px left-11 right-20 h-px bg-gradient-to-r from-blue-400/0 via-blue-400 to-blue-400/0" />
+                <div className="pl-4 pt-4">
+                  <TrafficLightsIcon className="h-2.5 w-auto stroke-slate-500/30" />
+                  <div className="mt-4 flex space-x-2 text-xs">
+                    {tabs.map((tab) => (
+                      <div
+                        key={tab.name}
+                        className={clsx(
+                          'flex h-6 rounded-full',
+                          tab.isActive
+                            ? 'bg-gradient-to-r from-sky-400/30 via-sky-400 to-sky-400/30 p-px font-medium text-sky-300'
+                            : 'text-slate-500'
+                        )}
+                      >
+                        <div
+                          className={clsx(
+                            'flex items-center rounded-full px-2.5',
+                            tab.isActive && 'bg-slate-800'
+                          )}
+                        >
+                          {tab.name}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="mt-6 flex items-start px-1 text-sm">
+                    <div
+                      aria-hidden="true"
+                      className="select-none border-r border-slate-300/5 pr-4 font-mono text-slate-600"
+                    >
+                      {Array.from({
+                        length: code.split('\n').length,
+                      }).map((_, index) => (
+                        <Fragment key={index}>
+                          {(index + 1).toString().padStart(2, '0')}
+                          <br />
+                        </Fragment>
+                      ))}
+                    </div>
+                    <Highlight
+                      {...defaultProps}
+                      code={code}
+                      language={codeLanguage}
+                      theme={undefined}
+                    >
+                      {({
+                        className,
+                        style,
+                        tokens,
+                        getLineProps,
+                        getTokenProps,
+                      }) => (
+                        <pre
+                          className={clsx(
+                            className,
+                            'flex overflow-x-auto pb-6'
+                          )}
+                          style={style}
+                        >
+                          <code className="px-4">
+                            {tokens.map((line, lineIndex) => (
+                              <div key={lineIndex} {...getLineProps({ line })}>
+                                {line.map((token, tokenIndex) => (
+                                  <span
+                                    key={tokenIndex}
+                                    {...getTokenProps({ token })}
+                                  />
+                                ))}
+                              </div>
+                            ))}
+                          </code>
+                        </pre>
+                      )}
+                    </Highlight>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
