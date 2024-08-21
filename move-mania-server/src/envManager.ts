5f84b65ca1957200e0256c82d36928f519596f9c
Max Mohammadi <maxmohammadi@gmail.com>
2024-08-20 19:48:46 -0700

    fix api url ssl wrong path error

--- move-mania-server/src/envManager.ts
+++ move-mania-server/src/envManager.ts
@@ -10,7 +10,7 @@ interface EnvConfig {
     MODULE_ADDRESS: string;
     CRASH_RESOURCE_ACCOUNT_ADDRESS: string;
     LP_RESOURCE_ACCOUNT_ADDRESS: string;
-    G_MOVE_RESOURCE_ACCOUNT_ADDRESS: string;
+    Z_APT_RESOURCE_ACCOUNT_ADDRESS: string;
 }
 
 function getConfig(): EnvConfig {
@@ -25,20 +25,20 @@ function getConfig(): EnvConfig {
         MODULE_ADDRESS,
         CRASH_RESOURCE_ACCOUNT_ADDRESS,
         LP_RESOURCE_ACCOUNT_ADDRESS,
-        G_MOVE_RESOURCE_ACCOUNT_ADDRESS
+        Z_APT_RESOURCE_ACCOUNT_ADDRESS
     } = process.env;
 
     if (!MOVEMENT_NODE || !APTOS_NODE || !CHAIN_MODE || !DEV_MODE ||
         !ADMIN_ACCOUNT_PRIVATE_KEY || !MODULE_ADDRESS || !CRASH_RESOURCE_ACCOUNT_ADDRESS ||
-        !LP_RESOURCE_ACCOUNT_ADDRESS || !G_MOVE_RESOURCE_ACCOUNT_ADDRESS) {
+        !LP_RESOURCE_ACCOUNT_ADDRESS || !Z_APT_RESOURCE_ACCOUNT_ADDRESS) {
         throw new Error('One or more environment variables are not defined');
 
 
     }
 
     const config: EnvConfig = {
-        NODE_URL: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
-        CHAIN_MODE: "Movement",
+        NODE_URL: "https://fullnode.testnet.aptoslabs.com",
+        CHAIN_MODE: "Aptos",
         ZION_API_URL: DEV_MODE === 'local' ? 'http://localhost:3008' : 'https://api.zionapi.xyz',
         ZION_APP_URL: DEV_MODE === 'local' ? 'http://localhost:3000' : 'https://app.zion.bet',
         DEV_MODE: DEV_MODE,
@@ -47,7 +47,7 @@ function getConfig(): EnvConfig {
         MODULE_ADDRESS: MODULE_ADDRESS as string,
         CRASH_RESOURCE_ACCOUNT_ADDRESS: CRASH_RESOURCE_ACCOUNT_ADDRESS as string,
         LP_RESOURCE_ACCOUNT_ADDRESS: LP_RESOURCE_ACCOUNT_ADDRESS as string,
-        G_MOVE_RESOURCE_ACCOUNT_ADDRESS: G_MOVE_RESOURCE_ACCOUNT_ADDRESS as string
+        Z_APT_RESOURCE_ACCOUNT_ADDRESS: Z_APT_RESOURCE_ACCOUNT_ADDRESS as string
     };
     console.log(config)
 
