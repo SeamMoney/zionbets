e7ea8ffb5617a62ec832660533358b88cff45e34
Max Mohammadi <maxmohammadi@gmail.com>
2024-08-20 19:35:28 -0700

    fix api url ssl wrong path error

--- move-mania-server/src/api.ts
+++ move-mania-server/src/api.ts
@@ -1,31 +1,33 @@
 import http from "http";
 import express from "express";
 import {
-  addChatMessage,
-  clearGames,
-  clearPlayerList,
-  createUser,
-  deleteUser,
-  getChatMessages,
-  getCurrentGame,
-  getGames,
-  getPlayerList,
-  getUser,
-  getUserByReferralCode,
-  getUsers,
-  hasUserBet,
-  hasUserCashOut,
-  updateUser,
+    addChatMessage,
+    clearGames,
+    clearPlayerList,
+    createUser,
+    deleteUser,
+    getChatMessages,
+    getCurrentGame,
+    getGames,
+    getPlayerList,
+    getUser,
+    getUserByReferralCode,
+    getUsers,
+    hasUserBet,
+    hasUserCashOut,
+    updateUser,
 } from "./database";
 import { io } from "socket.io-client";
 import { SOCKET_EVENTS } from "./types";
 require('dotenv').config();
 var cors = require("cors");
-const server = io("https://zionapi.xyz:8080")
+const server = io("https://api.zionapi.xyz:8080")
 const app = express();
 app.use(express.json());
+
 app.use(cors({
-  origin: process.env.ZION_APP_URL || "http://localhost:3000",
+    origin: [process.env.ZION_APP_URL, "http://localhost:3000", "https://zionapi.xyz", "https://api.zionapi.xyz"],
+    credentials: true
 }));
 
 const portHttp = 80;
@@ -34,24 +36,24 @@ const portHttps = 443;
 // USE FOR PRODUCTION
 import https from 'https';
 import fs from 'fs';
-const CERT_PATH = "/etc/letsencrypt/live/zionapi.xyz/fullchain.pem"
-const KEY_PATH = "/etc/letsencrypt/live/zionapi.xyz/privkey.pem"
+const CERT_PATH = "/etc/letsencrypt/live/api.zionapi.xyz/fullchain.pem";
+const KEY_PATH = "/etc/letsencrypt/live/api.zionapi.xyz/privkey.pem";
 const options = {
-  key: fs.readFileSync(KEY_PATH),
-  cert: fs.readFileSync(CERT_PATH)
+    key: fs.readFileSync(KEY_PATH),
+    cert: fs.readFileSync(CERT_PATH)
 };
 const httpsServer = https.createServer(options, app);
 httpsServer.listen(portHttps, () => {
-    console.log('HTTP Server running on port ', portHttps);
+    console.log('HTTP Server running on port ', portHttps);
     server.emit(SOCKET_EVENTS.START_ROUND)
 });
 
 
 /* 
-  This is the entry point for the server. 
+  This is the entry point for the server. 
 */
 app.get("/", (req, res) => {
-  res.send("Hello from the zion.bet server!");
+    res.send("Hello from the zion.bet server!");
 });
 
 /**
@@ -59,8 +61,8 @@ app.get("/", (req, res) => {
  */
 app.get("/users", async (req, res) => {
 
-  const users = await getUsers();
-  res.send(users);
+    const users = await getUsers();
+    res.send(users);
 });
 /**
  * This is the endpoint to get all games
@@ -68,115 +70,115 @@ app.get("/users", async (req, res) => {
 app.get("/games", async (req, res) => {
 
 
-  const games = await getGames();
-  res.send(games);
+    const games = await getGames();
+    res.send(games);
 });
 /**
  * This is the endpoint to get all playerlist
  */
 app.get("/playerlist", async (req, res) => {
 
-  const playerList = await getPlayerList();
-  res.send(playerList);
+    const playerList = await getPlayerList();
+    res.send(playerList);
 });
 /**
  * This is the endpoint to get all chat messages
  */
 app.get("/chat", async (req, res) => {
 
-  const chatMessages = await getChatMessages();
-  res.send(chatMessages);
+    const chatMessages = await getChatMessages();
+    res.send(chatMessages);
 });
 
 app.get("/users/referral/code/:code", async (req, res) => {
-  const code = req.params.code;
-  const user = await getUserByReferralCode(code);
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send("User not found");
-  }
+    const code = req.params.code;
+    const user = await getUserByReferralCode(code);
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404).send("User not found");
+    }
 });
 
 app.get("/users/:email", async (req, res) => {
 
-  const email = req.params.email;
-  const user = await getUser(email);
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send("User not found");
-  }
+    const email = req.params.email;
+    const user = await getUser(email);
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404).send("User not found");
+    }
 });
 
 app.get("/games/current", async (req, res) => {
 
-  const game = await getCurrentGame();
-  res.send(game);
+    const game = await getCurrentGame();
+    res.send(game);
 });
 
 app.delete("/games", async (req, res) => {
 
-  await clearGames();
-  res.send("Games cleared");
+    await clearGames();
+    res.send("Games cleared");
 });
 
 app.delete("/playerlist", async (req, res) => {
 
-  await clearPlayerList();
-  res.send("Player list cleared");
+    await clearPlayerList();
+    res.send("Player list cleared");
 });
 
 app.get("/playerlist/:email/hasbet", async (req, res) => {
 
-  const email = req.params.email;
-  const hasBet = await hasUserBet(email);
-  res.send(hasBet);
+    const email = req.params.email;
+    const hasBet = await hasUserBet(email);
+    res.send(hasBet);
 });
 
 app.get("/playerlist/:email/hascashout", async (req, res) => {
 
-  const email = req.params.email;
-  const hasCashOut = await hasUserCashOut(email);
-  res.send(hasCashOut);
+    const email = req.params.email;
+    const hasCashOut = await hasUserCashOut(email);
+    res.send(hasCashOut);
 });
 
 app.post("/users", async (req, res) => {
 
-  const user = req.body;
-  try {
-    await createUser(user);
-  } catch (e) {
-    res.status(400).send("User already exists");
-    return;
-  }
-  res.send("User created");
+    const user = req.body;
+    try {
+        await createUser(user);
+    } catch (e) {
+        res.status(400).send("User already exists");
+        return;
+    }
+    res.send("User created");
 });
 
 app.post("/chat", async (req, res) => {
 
-  const chatMessage = req.body;
-  await addChatMessage(chatMessage);
-  res.send("Chat message added");
+    const chatMessage = req.body;
+    await addChatMessage(chatMessage);
+    res.send("Chat message added");
 });
 
 app.put("/users/:email", async (req, res) => {
 
-  const email = req.params.email;
-  const user = req.body;
-  await updateUser(email, user);
-  res.send("User updated");
+    const email = req.params.email;
+    const user = req.body;
+    await updateUser(email, user);
+    res.send("User updated");
 });
 
 app.delete("/users/:email", async (req, res) => {
 
-  const email = req.params.email;
-  await deleteUser(email);
-  res.send("User deleted");
+    const email = req.params.email;
+    await deleteUser(email);
+    res.send("User deleted");
 });
 
-/* 
-  The server will listen on port PORT
+/*
+  The server will listen on port PORT
 */
 // app.listen(portHttp, () => {
 //   console.log("Server is running on port " + portHttp);
--- move-mania-server/src/server.ts
+++ move-mania-server/src/server.ts
@@ -1,15 +1,15 @@
 import http from "http";
-import {AptosClient} from "aptos";
+import { AptosClient } from "aptos";
 import { Server } from "socket.io";
 import { ChatMessage, SOCKET_EVENTS } from "./types";
 import {
-  addBetToPlayerList,
-  addCashOutToPlayerList,
-  addChatMessage,
-  clearPlayerList,
-  createGame,
-  endGame,
-  getUser,
+  addBetToPlayerList,
+  addCashOutToPlayerList,
+  addChatMessage,
+  clearPlayerList,
+  createGame,
+  endGame,
+  getUser,
 } from "./database";
 import express from "express";
 
@@ -31,156 +31,156 @@ app.use(cors());
 
 const portHttps = 8080;
 
-const aptosClient = new AptosClient(`${ process.env.APTOS_NODE}/v1`);
+const aptosClient = new AptosClient(`${process.env.APTOS_NODE}/v1`);
 
 // USE FOR PRODUCTION
 import https from 'https';
 import fs from 'fs';
-const CERT_PATH = "/etc/letsencrypt/live/zionapi.xyz/fullchain.pem"
-const KEY_PATH = "/etc/letsencrypt/live/zionapi.xyz/privkey.pem"
+const CERT_PATH = "/etc/letsencrypt/live/api.zionapi.xyz/fullchain.pem"
+const KEY_PATH = "/etc/letsencrypt/live/api.zionapi.xyz/privkey.pem"
 const options = {
-  key: fs.readFileSync(KEY_PATH),
-  cert: fs.readFileSync(CERT_PATH)
+  key: fs.readFileSync(KEY_PATH),
+  cert: fs.readFileSync(CERT_PATH)
 };
 const httpsServer = https.createServer(options, app);
 httpsServer.listen(portHttps, () => {
-    console.log('HTTPs Server running on port ', portHttps);
+  console.log('HTTPs Server running on port ', portHttps);
 });
 
 const io = new Server(httpsServer, {
-  cors: {
-    origin: process.env.ZION_APP_URL || "http://localhost:3000",
-    credentials: true,
-  },
-  connectionStateRecovery: {}
+  cors: {
+    origin: process.env.ZION_APP_URL ? [process.env.ZION_APP_URL, "http://localhost:3000", "https://zionapi.xyz", "https://api.zionapi.xyz"] : true,
+    credentials: true,
+  },
+  connectionStateRecovery: {}
 });
 
 // httpsServer.listen(portHttps, () => {
 //   console.log("Server running on port:", portHttps);
 // });
 
-const getDateNow = async()=>{
+const getDateNow = async () => {
   const ledgerInfo = await aptosClient.getLedgerInfo();
   const timestampInMicroseconds = ledgerInfo.ledger_timestamp;
-  return Math.floor(parseInt(timestampInMicroseconds)/1000);
+  return Math.floor(parseInt(timestampInMicroseconds) / 1000);
 }
 
 io.on("connection", (socket) => {
-  socket.on(SOCKET_EVENTS.SET_BET, async (betData) => {
-    await addBetToPlayerList(betData);
-    io.emit(SOCKET_EVENTS.BET_CONFIRMED, betData);
-  });
+  socket.on(SOCKET_EVENTS.SET_BET, async (betData) => {
+    await addBetToPlayerList(betData);
+    io.emit(SOCKET_EVENTS.BET_CONFIRMED, betData);
+  });
 
-  socket.on(SOCKET_EVENTS.CASH_OUT, async (cashOutData) => {
-    await addCashOutToPlayerList(cashOutData);
-    io.emit(SOCKET_EVENTS.CASH_OUT_CONFIRMED, cashOutData);
-  });
+  socket.on(SOCKET_EVENTS.CASH_OUT, async (cashOutData) => {
+    await addCashOutToPlayerList(cashOutData);
+    io.emit(SOCKET_EVENTS.CASH_OUT_CONFIRMED, cashOutData);
+  });
 
-  socket.on(SOCKET_EVENTS.START_ROUND, async () => {
+  socket.on(SOCKET_EVENTS.START_ROUND, async () => {
     console.log("CREATE")
-    await clearPlayerList();
+    await clearPlayerList();
 
-    let blockchainTxn = await createNewGame('house_secret', 'salt')
-    console.log("blockchainTxn", blockchainTxn)
-    
-    if (blockchainTxn === null) {
-      console.error("Error creating new game")
-      return
-    }
-    
-    const crashPoint = calculateCrashPoint(blockchainTxn.randomNumber, 'house_secretsalt');
-    
-    const startTime = Math.floor(blockchainTxn.startTime / 1000);
-    const gameId = Math.random().toString(36).substring(7);
-    await createGame({
-      game_id: gameId,
-      start_time: startTime,
-      secret_crash_point: crashPoint,
-      status: "IN_PROGRESS",
-    });
-    console.log("start rounded")
-    io.emit(SOCKET_EVENTS.ROUND_START, {
-      roundId: 1,
-      startTime: startTime,
-      crashPoint,
-    });
+    let blockchainTxn = await createNewGame('house_secret', 'salt')
+    console.log("blockchainTxn", blockchainTxn)
+
+    if (blockchainTxn === null) {
+      console.error("Error creating new game")
+      return
+    }
+
+    const crashPoint = calculateCrashPoint(blockchainTxn.randomNumber, 'house_secretsalt');
+
+    const startTime = Math.floor(blockchainTxn.startTime / 1000);
+    const gameId = Math.random().toString(36).substring(7);
+    await createGame({
+      game_id: gameId,
+      start_time: startTime,
+      secret_crash_point: crashPoint,
+      status: "IN_PROGRESS",
+    });
+    console.log("start rounded")
+    io.emit(SOCKET_EVENTS.ROUND_START, {
+      roundId: 1,
+      startTime: startTime,
+      crashPoint,
+    });
 
     const now = await getDateNow();
-    console.log(startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now)
+    console.log(startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now)
 
-    setTimeout(async () => {
-      const blockchainTxn = await endGameAptos('house_secret', 'salt', startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000))
-      console.log("blockchainTxn", blockchainTxn)
+    setTimeout(async () => {
+      const blockchainTxn = await endGameAptos('house_secret', 'salt', startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000))
+      console.log("blockchainTxn", blockchainTxn)
 
-      if (blockchainTxn === null) {
-        console.error("Error ending game")
-        return
-      }
+      if (blockchainTxn === null) {
+        console.error("Error ending game")
+        return
+      }
 
-      await endGame(gameId);
-      console.log("end rounded")
-      io.emit(SOCKET_EVENTS.ROUND_RESULT, { roundId: gameId, crashPoint });
-      setTimeout(async () => {
-        await cycleRounds();
-      }, SUMMARY);
-    }, startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now + 100);
-  });
+      await endGame(gameId);
+      console.log("end rounded")
+      io.emit(SOCKET_EVENTS.ROUND_RESULT, { roundId: gameId, crashPoint });
+      setTimeout(async () => {
+        await cycleRounds();
+      }, SUMMARY);
+    }, startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now + 100);
+  });
 
-  socket.on(SOCKET_EVENTS.CHAT_MESSAGE, async (message) => {
-    await addChatMessage({
-      authorEmail: message.authorEmail,
-      message: message.message,
-    });
-    io.emit(SOCKET_EVENTS.CHAT_NOTIFICATION, message);
-  });
+  socket.on(SOCKET_EVENTS.CHAT_MESSAGE, async (message) => {
+    await addChatMessage({
+      authorEmail: message.authorEmail,
+      message: message.message,
+    });
+    io.emit(SOCKET_EVENTS.CHAT_NOTIFICATION, message);
+  });
 });
 
 async function cycleRounds() {
-  await clearPlayerList();
-  const now = await getDateNow();
-  let blockchainTxn = await createNewGame('house_secret', 'salt')
-  console.log("blockchainTxn", blockchainTxn)
-    
-  if (blockchainTxn === null) {
-    console.error("Error creating new game")
-    return
-  }
-  
-  const crashPoint = calculateCrashPoint(blockchainTxn.randomNumber, 'house_secretsalt');
-  
-  const startTime = Math.floor(blockchainTxn.startTime / 1000);
-  const gameId = Math.random().toString(36).substring(7);
-  await createGame({
-    game_id: gameId,
-    start_time: startTime,
-    secret_crash_point: crashPoint,
-    status: "IN_PROGRESS",
-  });
-  console.log("start rounded")
-  io.emit(SOCKET_EVENTS.ROUND_START, {
-    roundId: 1,
-    startTime: startTime,
-    crashPoint,
-  });
+  await clearPlayerList();
+  const now = await getDateNow();
+  let blockchainTxn = await createNewGame('house_secret', 'salt')
+  console.log("blockchainTxn", blockchainTxn)
 
-  console.log(startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now)
+  if (blockchainTxn === null) {
+    console.error("Error creating new game")
+    return
+  }
 
-  setTimeout(async () => {
-    const blockchainTxn = await endGameAptos('house_secret', 'salt', startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000))
-    console.log("blockchainTxn", blockchainTxn)
+  const crashPoint = calculateCrashPoint(blockchainTxn.randomNumber, 'house_secretsalt');
 
-    if (blockchainTxn === null) {
-      console.error("Error ending game")
-      return
-    }
-    
-    await endGame(gameId);
-    console.log("end rounded")
-    io.emit(SOCKET_EVENTS.ROUND_RESULT, { roundId: gameId, crashPoint });
-    setTimeout(async () => {
-      await cycleRounds();
-    }, SUMMARY);
-  }, startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now + 100);
+  const startTime = Math.floor(blockchainTxn.startTime / 1000);
+  const gameId = Math.random().toString(36).substring(7);
+  await createGame({
+    game_id: gameId,
+    start_time: startTime,
+    secret_crash_point: crashPoint,
+    status: "IN_PROGRESS",
+  });
+  console.log("start rounded")
+  io.emit(SOCKET_EVENTS.ROUND_START, {
+    roundId: 1,
+    startTime: startTime,
+    crashPoint,
+  });
+
+  console.log(startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now)
+
+  setTimeout(async () => {
+    const blockchainTxn = await endGameAptos('house_secret', 'salt', startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000))
+    console.log("blockchainTxn", blockchainTxn)
+
+    if (blockchainTxn === null) {
+      console.error("Error ending game")
+      return
+    }
+
+    await endGame(gameId);
+    console.log("end rounded")
+    io.emit(SOCKET_EVENTS.ROUND_RESULT, { roundId: gameId, crashPoint });
+    setTimeout(async () => {
+      await cycleRounds();
+    }, SUMMARY);
+  }, startTime + ((crashPoint == 0 ? 0 : log(EXPONENTIAL_FACTOR, crashPoint)) * 1000) - now + 100);
 }
 
 
@@ -192,8 +192,8 @@ async function cycleRounds() {
  * @returns The logarithm of the value with the given base
  */
 function log(base: number, value: number): number {
-  console.log("log base:", base)
-  console.log("log value:", value)
-  console.log("log result:", Math.log(value) / Math.log(base))
-  return Math.log(value) / Math.log(base);
+  console.log("log base:", base)
+  console.log("log value:", value)
+  console.log("log result:", Math.log(value) / Math.log(base))
+  return Math.log(value) / Math.log(base);
 }
