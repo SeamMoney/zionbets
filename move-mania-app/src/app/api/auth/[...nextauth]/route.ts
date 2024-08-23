b36536221f29b783524746d3d6ddbfc30cd5decc
Max Mohammadi <maxmohammadi@gmail.com>
2024-08-22 17:12:40 -0700

    api url fix

--- move-mania-app/src/app/api/auth/[...nextauth]/route.ts
+++ move-mania-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { setUpAndGetUser } from "@/lib/api"
 
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET || 'secret',
@@ -9,7 +10,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     })
   ],
-  debug: true, // Enable debug mode
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (!user.email) {
+        console.error("User email is missing")
+        return false
+      }
+
+      try {
+        const newUser = await setUpAndGetUser({
+          username: user.name || "",
+          image: user.image || "",
+          email: user.email,
+          referral_code: "",
+          referred_by: null,
+        })
+
+        if (!newUser) {
+          console.error("Failed to set up user")
+          return false
+        }
+
+        return true
+      } catch (error) {
+        console.error("Error in sign in callback:", error)
+        return false
+      }
+    },
+  },
+  debug: true,
   logger: {
     error(code, metadata) {
       console.error(code, metadata)
